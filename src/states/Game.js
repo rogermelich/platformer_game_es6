302ba4f9629b25c6d5ca756a59403feb4fcef14e
Roger Melich <rogermelich@gmail.com>
2017-05-05 16:37:56 +0200

    And Physics

--- src/states/Game.js
+++ src/states/Game.js
@@ -3,22 +3,36 @@ import Phaser from 'phaser'
 import Mushroom from '../sprites/Mushroom'
 
 export default class extends Phaser.State {
-  init () {}
-  preload () {
-    this.game.load.spritesheet('player', './assets/player.png')
-  }
-
-  create () {
-    this.game.physics.startSystem(Phaser.Physics.ARCADE)
-    this.player = this.game.add.sprite(250,50, 'player')
-      
-    game.physics.arcade.enable(this.player)
-    this.player.body.gravity.y = 600
-  }
-
-  render () {
-    if (__DEV__) {
-      // this.game.debug.spriteInfo(this.mushroom, 32, 32)
+    init() {
     }
-  }
+
+    preload() {
+        this.game.load.spritesheet('player', './assets/player.png', 28, 22)
+        this.game.load.image('ground', './assets/ground.png')
+    }
+
+    create() {
+        this.game.physics.startSystem(Phaser.Physics.ARCADE)
+        this.player = this.game.add.sprite(250, 50, 'player')
+
+        this.game.add.sprite(760 / 2 - 160, 400 / 2, 'ground')
+
+        game.physics.arcade.enable(this.player)
+        this.player.body.gravity.y = 600
+
+        this.player.body.setSize(20, 20, 0, 0);
+
+        this.player.animations.add('idle', [3, 4, 5, 4], 5, true)
+        this.player.animations.play('idle')
+    }
+
+    update() {
+        this.game.physics.arcade.collide(this.player, this.ground)
+    }
+
+    render() {
+        if (__DEV__) {
+            // this.game.debug.spriteInfo(this.mushroom, 32, 32)
+        }
+    }
 }

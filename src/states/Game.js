64bdacbb5a92fc9f4668016fded71a8e2b9cac34
Roger Melich <rogermelich@gmail.com>
2017-05-05 15:46:54 +0200

    Initial version

---
+++ src/states/Game.js
@@ -0,0 +1,20 @@
+/* globals __DEV__ */
+import Phaser from 'phaser'
+import Mushroom from '../sprites/Mushroom'
+
+export default class extends Phaser.State {
+  init () {}
+  preload () {
+
+  }
+
+  create () {
+
+  }
+
+  render () {
+    if (__DEV__) {
+      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+    }
+  }
+}

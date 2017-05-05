133ca98e7c65d73b6d2a9fc09de2cd00722aeeaa
Roger Melich <rogermelich@gmail.com>
2017-05-05 16:10:22 +0200

    Add Assets and load player and Physics

--- src/states/Game.js
+++ src/states/Game.js
@@ -5,16 +5,20 @@ import Mushroom from '../sprites/Mushroom'
 export default class extends Phaser.State {
   init () {}
   preload () {
-
+    this.game.load.spritesheet('player', './assets/player.png')
   }
 
   create () {
-
+    this.game.physics.startSystem(Phaser.Physics.ARCADE)
+    this.player = this.game.add.sprite(250,50, 'player')
+      
+    game.physics.arcade.enable(this.player)
+    this.player.body.gravity.y = 600
   }
 
   render () {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+      // this.game.debug.spriteInfo(this.mushroom, 32, 32)
     }
   }
 }

b4579fae99931d81746fac6e46f9c4a293b6190d
Roger Melich <rogermelich@gmail.com>
2017-05-09 19:22:39 +0200

    Physics, wall and jumpSound ok

--- src/states/Game.js
+++ src/states/Game.js
@@ -16,40 +16,71 @@ export default class extends Phaser.State {
 
     create() {
         this.game.physics.startSystem(Phaser.Physics.ARCADE)
-        this.player = this.game.add.sprite(250, 50, 'player')
 
-        this.game.add.sprite(760 / 2 - 160, 400 / 2, 'ground')
+        this.player = this.game.add.sprite(360, 100, 'player')
+        this.ground = this.game.add.sprite(760 / 2 - 160, 400 / 2, 'ground')
+        this.wall1 = this.game.add.sprite(760/2-160, 400/2-80, 'wall')
+        this.wall2 = this.game.add.sprite(760/2+140, 400/2-80, 'wall')
+
+        this.jumpSound = this.game.add.audio('jump', 0.1)
 
         game.physics.arcade.enable(this.player)
+        game.physics.arcade.enable(this.ground)
+        game.physics.arcade.enable(this.wall1)
+        game.physics.arcade.enable(this.wall2)
         this.player.body.gravity.y = 600
 
         this.player.body.setSize(20, 20, 0, 0);
+
+        this.ground.body.immovable = true
+        this.wall1.body.immovable = true
+        this.wall2.body.immovable = true
 
         this.player.animations.add('idle', [3, 4, 5, 4], 5, true)
         this.player.animations.play('idle')
 
         this.cursor = game.input.keyboard.createCursorKeys()
+
+        this.hasJumped = false
     }
 
     update() {
         this.game.physics.arcade.collide(this.player, this.ground)
+        this.game.physics.arcade.collide(this.player, this.wall1)
+        this.game.physics.arcade.collide(this.player, this.wall2)
 
         this.inputs
+
+        if (this.player.body.touching.down) {
+            this.hasJumped = false
+        }
     }
 
     inputs() {
-        if(this.cursor.left.isLeft){
-            this.player.velocity.x = -100
+        if (this.cursor.left.isDown || this.moveLeft) {
+            this.player.body.velocity.x = -200
+            this.player.frame = 2
+        }
+        else if (this.cursor.right.isDown || this.moveRight) {
+            this.player.body.velocity.x = 200
+            this.player.frame = 1
+        }
+        else {
+            this.player.body.velocity.x = 0
         }
 
-        if(this.cursor.right.isRight){
-            this.player.velocity.x = 100
+        if (this.cursor.up.isDown) {
+            this.jumpPlayer()
         }
     }
 
     jumpPlayer() {
-        this.player.body.velocity.y = - 200
-        this.jump
+        this.player.body.velocity.y = -220
+
+        if (!this.hasJumped) {
+            this.jumpSound.play()
+            this.hasJumped = true
+        }
     }
 
     render() {
